4c27926a144c17c8dfb6a0d28491177284bbe06c
Victória Amaral <ttoiav.aamaral@gmail.com>
2024-07-06 11:54:16 -0300

    selects(cliente): deixando codigo limpo.

--- Projeto Comercio/Selects/Cliente.sql
+++ Projeto Comercio/Selects/Cliente.sql
@@ -1,23 +1,57 @@
 use comercio;
 
---- Select para tabela cliente
+--- Select para tabela cliente!
+
+-- Para ver todos os valores da tabela;
 SELECT * FROM cliente;
+
+-- Para ver clientes sem nome social;
 SELECT * FROM cliente WHERE nomeSocial= 'N\A';
+
+-- Para ver clientes que o nome se inicia com a letra G;
 SELECT * FROM cliente WHERE nome like 'G%';
+
+-- Para ver clientes que o nome termina com a letra G;
 SELECT * FROM cliente WHERE nome like '%A';
+
+-- Para ver clientes que o nome se inicia com a letra G e termina com a letra a;
 SELECT * FROM cliente WHERE nome like 'G%' AND nome like '%a';
+
+-- Para ver clientes que o tipo de documento corresponde a CP;
 SELECT * FROM cliente WHERE tipoDocumento = 'CP';
+
+-- Ver nome e data de nascimento dos clientes;
 SELECT nome, dataNascimento FROM cliente;
-SELECT COUNT(*) AS total_clientes FROM cliente;
-SELECT 8 FROM cliente ORDER BY nome ASC;
-SELECT 8 FROM cliente ORDER BY nome DESC;
-SELECT * FROM cliente WHERE nomeSocial IS NULL;
 
-SELECT cliente.id_cliente, cliente.nome, contato.telefone, contato.celular
-FROM cliente INNER JOIN contato ON cliente.id_cliente = contato.id_cliente;
+-- Contagem de clientes;
+SELECT COUNT(*) AS total_clientes
+FROM cliente;
 
-SELECT cliente.id_cliente, cliente.nome, contato.telefone, contato.celular,
-    endereco.logradouro, endereco.numero, endereco.bairro, endereco.cidade
-    FROM cliente
-    INNER JOIN contato ON cliente.id_cliente = contato.id_cliente
-    INNER JOIN endereco ON cliente.id_cliente = endereco.id_cliente; 
+-- Ver 8 registros ordenados por nome ASC;
+SELECT *
+FROM cliente
+ORDER BY nome ASC
+LIMIT 8;
+
+-- Ver 8 registros ordenados por nome DESC;
+SELECT *
+FROM cliente
+ORDER BY nome DESC
+LIMIT 8;
+
+-- Ver clientes onde nomeSocial é NULL;
+SELECT *
+FROM cliente
+WHERE nomeSocial IS NULL;
+
+-- Detalhes de cliente com informações de contato;
+SELECT c.id_cliente, c.nome, ct.telefone, ct.celular
+FROM cliente c
+INNER JOIN contato ct ON c.id_cliente = ct.id_cliente;
+
+-- Detalhes completos de cliente com contato e endereço;
+SELECT c.id_cliente, c.nome, ct.telefone, ct.celular,
+       e.logradouro, e.numero, e.bairro, e.cidade
+    FROM cliente c
+    INNER JOIN contato ct ON c.id_cliente = ct.id_cliente
+    INNER JOIN endereco e ON c.id_cliente = e.id_cliente;
