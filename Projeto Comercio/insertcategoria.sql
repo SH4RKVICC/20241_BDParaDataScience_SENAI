66e7a0b24383228979dc9a6de4f6123f689f9f34
Victória Amaral <ttoiav.aamaral@gmail.com>
2024-07-06 10:35:26 -0300

    projeto-comercio(insert-categoria): inserindo valores na tabela categoria.

---
+++ Projeto Comercio/insertcategoria.sql
@@ -0,0 +1,17 @@
+use comercio;
+
+-- Inserção de 10 registros na tabela categoria
+INSERT INTO categoria (nomeCategoria) VALUES
+('Monitores'),
+('Teclados'),
+('Mouses'),
+('Impressoras'),
+('Placas Mãe'),
+('Memórias RAM'),
+('Discos Rígidos'),
+('SSD'),
+('Fontes de Alimentação'),
+('Gabinetes');
+
+-- Visualizar os dados inseridos
+SELECT * FROM categoria;
