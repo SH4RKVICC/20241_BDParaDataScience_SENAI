3fc2f86bc553c6ec014676923537a1720dae773b
Victória Amaral <ttoiav.aamaral@gmail.com>
2024-07-17 16:20:28 -0300

    projeto-livraria(tabelas): criando rascunho de tb_livro.

---
+++ Projeto Livraria/Tabelas/tb_livro.sql
@@ -0,0 +1,15 @@
+CREATE TABLE tb_livro(
+	id_livro SMALLINT IDENTITY PRIMARY KEY,
+	id_editora SMALLINT,
+	id_fornecedor SMALLINT,
+	nomeProduto VARCHAR(100),
+	marca VARCHAR(100),
+	valor FLOAT(5),
+	descricao VARCHAR(250),
+	CONSTRAINT fk_produto_categoria FOREIGN KEY (id_categoria)
+	REFERENCES categoria(id_categoria),
+	CONSTRAINT fk_produto_fornecedor FOREIGN KEY (id_fornecedor)
+	REFERENCES fornecedor(id_fornecedor),
+);
+
+SELECT * FROM tb_livro;
