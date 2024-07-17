0e297f2fbb97a7bb44107e7245d5420b38bcc363
Victória Amaral <ttoiav.aamaral@gmail.com>
2024-07-17 16:32:20 -0300

    projeto-livraria(tabelas): criando tb_livro.

--- Projeto Livraria/Tabelas/tb_livro.sql
+++ Projeto Livraria/Tabelas/tb_livro.sql
@@ -6,6 +6,8 @@ CREATE TABLE tb_livro(
 	REFERENCES tb_editora(id_editora),
 	CONSTRAINT fk_livro_genero FOREIGN KEY (id_genero)
 	REFERENCES tb_genero(id_genero),
+    CONSTRAINT fk_livro_autor FOREIGN KEY (id_autor)
+	REFERENCES tb_autor(id_autor)
 );
 
 SELECT * FROM tb_livro;
