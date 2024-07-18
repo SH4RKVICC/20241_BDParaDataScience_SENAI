f16f20f344b1ad021ce83d80188294380456f5a2
Victória Amaral <ttoiav.aamaral@gmail.com>
2024-07-18 00:21:57 -0300

    projeto-livraria: inserindo valores em tb_devolucao e tb_emprestimo.

---
+++ Projeto Livraria/Inserts/insert_devolucao.sql
@@ -0,0 +1,18 @@
+-- Inserção de 15 tuplas na tabela tb_devolucao
+INSERT INTO tb_devolucao (id_emprestimo, dataDevolucao, devolvidoPrazo)
+VALUES
+(1, '2024-07-24', CAST(1 AS BIT)),
+(2, '2024-07-24', CAST(1 AS BIT)),
+(3, '2024-07-24', CAST(1 AS BIT)),
+(4, '2024-07-24', CAST(1 AS BIT)),
+(5, '2024-07-24', CAST(1 AS BIT)),
+(6, '2024-07-24', CAST(1 AS BIT)),
+(7, '2024-07-24', CAST(1 AS BIT)),
+(8, '2024-07-24', CAST(1 AS BIT)),
+(9, '2024-07-24', CAST(1 AS BIT)),
+(10, '2024-07-24', CAST(1 AS BIT)),
+(11, '2024-07-24', CAST(1 AS BIT)),
+(12, '2024-07-24', CAST(1 AS BIT)),
+(13, '2024-07-24', CAST(1 AS BIT)),
+(14, '2024-07-24', CAST(1 AS BIT)),
+(15, '2024-07-24', CAST(1 AS BIT));
